02c92b760382b4ccaa7af2542cb625bc77ddd820
Nelson Mak <nelmak@amazon.com>
2024-11-21 22:50:54 -0800

    delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,9 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${
+      matchingProduct.id
+    }">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -105,6 +107,10 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     // remove product from cart then update HTML
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
