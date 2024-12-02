0e9d10c988229a2faec2623c8bf625ef93f3983f
Nelson Mak <nelmak@amazon.com>
2024-12-02 10:44:47 -0800

    remove default test and add money tests

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,8 +1,8 @@
 import { formatCurrency } from "../scripts/utils/money.js";
 
-console.log('test suite: formatCurrency');
+console.log("test suite: formatCurrency");
 
-console.log('converts cents into dollars');
+console.log("converts cents into dollars");
 
 if (formatCurrency(2095) === "20.95") {
   console.log("passed");
@@ -10,7 +10,7 @@ if (formatCurrency(2095) === "20.95") {
   console.log("failed");
 }
 
-console.log('works with 0')
+console.log("works with 0");
 
 if (formatCurrency(0) === "0.00") {
   console.log("passed");
@@ -18,14 +18,10 @@ if (formatCurrency(0) === "0.00") {
   console.log("failed");
 }
 
-
-console.log('rounds up to the nearest cent')
+console.log("rounds up to the nearest cent");
 
 if (formatCurrency(2000.5) === "20.01") {
   console.log("passed");
 } else {
   console.log("failed");
 }
-
-
-
