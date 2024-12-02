c13e1437db8a2283c87a7751b0b813c5e75130d7
Nelson Mak <nelmak@amazon.com>
2024-12-02 11:11:40 -0800

    create automated tests for addToCart

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,23 +1,43 @@
-import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
+import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
 
-describe('test suite: addToCart', () => {
-    it('adds an existing product to the cart', () => {
-        
+describe("test suite: addToCart", () => {
+  it("adds an existing product to the cart", () => {
+    spyOn(localStorage, "setItem");
+
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([
+        {
+          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+          quantity: 1,
+          deliverOptionId: "1",
+        },
+      ]);
     });
+    loadFromStorage();
 
-    it('adds a new product to the cart', () => {
-        spyOn(localStorage, 'setItem');
+    addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    expect(cart[0].quantity).toEqual(2);
+  });
 
-        // mock
-        spyOn(localStorage, 'getItem').and.callFake(() => {
-            return JSON.stringify([]);
-        });
-        loadFromStorage();
+  it("adds a new product to the cart", () => {
+    // first param is obj we want to mock, then string is method we want to mock
+    spyOn(localStorage, "setItem");
 
-        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-        expect(cart.length).toEqual(1);
-        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-        expect(cart[0].quantity).toEqual(1);
+    // mock - replace a method with a fake version
+    // make fake version do whatever we want - e.g. use mock to create fake version of getItem
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      // this is what we want getItem to do
+      return JSON.stringify([]);
     });
+    loadFromStorage();
+
+    addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    expect(cart[0].quantity).toEqual(1);
+  });
 });
