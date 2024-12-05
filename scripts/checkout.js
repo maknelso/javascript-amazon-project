f5370bed5f4f9b7712aa31c766b17e413340c5d4
Nelson Mak <nelmak@amazon.com>
2024-12-04 17:00:52 -0800

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,56 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
-loadProducts(() => {
-  renderOrderSummary();
-  renderPaymentSummary();
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      // This will run after loadProducts is finished
+      resolve("value1");
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values) => {
+    console.log(values)
+    renderOrderSummary();
+    renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    // This will run after loadProducts is finished
+    resolve("value1");
+  });
+})
+  .then((value) => {
+    console.log(value);
+
+    return new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+    });
+  })
+  .then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+*/
+
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
